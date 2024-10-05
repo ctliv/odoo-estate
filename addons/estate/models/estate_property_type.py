04d50e6234d2b8e6789c30c9b1f38997c0b37ec1
Cristiano Toncelli <ct.livorno@gmail.com>
2024-10-05 18:36:23 +0000

    Chapter 11

--- addons/estate/models/estate_property_type.py
+++ addons/estate/models/estate_property_type.py
@@ -1,15 +1,23 @@
-from odoo import fields, models
+from odoo import api, fields, models
+
 
 class EstatePropertyType(models.Model):
-    _name = 'estate.property.type'
-    _description = 'Estate Property Type'
+    _name = "estate.property.type"
+    _description = "Estate Property Type"
     _order = "name asc"
 
-    name = fields.Char('Property Type Name', required=True)
-    sequence = fields.Integer('Sequence', default=1, help="Order sequence")
-    active = fields.Boolean('Active', default=True)
-    property_ids = fields.One2many("estate.property", "type_id")
+    name = fields.Char("Property Type Name", required=True)
+    sequence = fields.Integer("Sequence", default=1, help="Order sequence")
+    active = fields.Boolean("Active", default=True)
+    property_ids = fields.One2many("estate.property", inverse_name="type_id")
+    offer_ids = fields.One2many(
+        "estate.property.offer", "property_type_id"
+    )
+    offer_count = fields.Integer("Offers", compute="_compute_offer_count")
 
-    _sql_constraints = [
-        ('unique_type', 'unique(name)', "Type already exists.")
-    ]
+    _sql_constraints = [("unique_type", "unique(name)", "Type already exists.")]
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
